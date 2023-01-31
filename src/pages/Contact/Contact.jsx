cda6baf879b69a037eb62fed75843250bf11757b
justinpastine <justin.pastine@gmail.com>
2023-01-31 17:56:15 -0500

    add projectdetails folder and file

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,6 +1,13 @@
 const Contact = () => {
   return (
-    <h1>Contact</h1>
+    <>
+      <h1>Contact</h1>
+      <p>Please reach out if you'd like to work with me!</p>
+      <a href="mailto: justin.pastine@gmail.com">Send Email</a>
+    
+      <a href="https://github.com/jpastine">My GitHub</a>
+      <a href="https://www.linkedin.com/in/justin-pastine/">My LinkedIn Profile</a>
+    </>
   )
 }
 
