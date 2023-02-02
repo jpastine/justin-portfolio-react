70956f3b57f0e631b0fd2923b09c8a4ae8d51250
justinpastine <justin.pastine@gmail.com>
2023-02-01 19:13:46 -0500

    add hyphenate functionality

--- src/pages/ProjectDetails/ProjectDetails.js
+++ src/pages/ProjectDetails/ProjectDetails.js
@@ -1,9 +1,12 @@
 import { projectsData } from "../../data/projects"
+import { findProject } from "../../utilities/findProject"
+import { useParams } from "react-router-dom"
 
 const ProjectDetails = () => {
+  const projectId = useParams()
   return (
     <>
-      <h1>{projectsData[0].title}</h1>
+      <h1>{projectsData.title}</h1>
       <p>{projectsData.description}</p>
       <img src="/screenshot.jpeg" alt="" />
       <a href="https://github.com/jpastine/blackjack.git">GitHub Link</a>
