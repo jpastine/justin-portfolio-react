5b8355f6d6b024b7271be05ea01fb85d3e075b0a
justinpastine <justin.pastine@gmail.com>
2023-03-07 14:15:08 -0500

    add redirects folder to stop refresh from crashing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route} from 'react-router-dom'
+import { Routes, Route,} from 'react-router-dom'
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import Contact from './pages/Contact/Contact';
@@ -8,14 +8,15 @@ import Projects from './pages/Projects/Projects';
 import NavBar from './components/NavBar';
 import ProjectDetails from './pages/ProjectDetails/ProjectDetails';
 
+
 function App() {
   return (
     <>
       <NavBar />
       <Routes>
         <Route path='/' element={<Home /> } />
-        <Route path='about' element={<About />} />
-        <Route path='contact' element={<Contact />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
         <Route path='/resume' element={<Resume />} />
         <Route path='/projects' element={<Projects />} />
         <Route path='/projects/:projectDetails' element={<ProjectDetails />} />
